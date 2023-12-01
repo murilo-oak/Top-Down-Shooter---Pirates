89c897d0e1c0a620c3847a41ae74c1802b626b23
unknown <murilo63793@gmail.com>
2023-12-01 15:23:33 -0300

    refactor: parse into smaller functions

--- Assets/Enemies/Scripts/EnemySpawner.cs
+++ Assets/Enemies/Scripts/EnemySpawner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem.HID;
 
 
 public class EnemySpawner : MonoBehaviour
@@ -18,82 +19,122 @@ public class EnemySpawner : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(SpawnEnemy());
         frequencyToSpawnNewEnemy = frequencySpawn.timerLengthSeconds;
-        if(frequencyToSpawnNewEnemy <= 0)
-            frequencyToSpawnNewEnemy = 1;
+        ValidateSpawnFrequency();
+        StartCoroutine(SpawnEnemy());
     }
 
     IEnumerator SpawnEnemy()
     {
-        float randomIndexFloat = Random.Range(0, enemyTypes.Count);
-        int randomIndex = Mathf.FloorToInt(randomIndexFloat);
-
         Vector3 point;
         
-        if (TryCreateRandomPoint(out point))
+        if (TryToFindRandomPointInWorldToSpawn(out point))
         {
-            Quaternion RandomRotationAroundZ = Quaternion.Euler(0, 0, Random.Range(0f, 360f));
-            Instantiate(enemyTypes[randomIndex], point, RandomRotationAroundZ);
+            InstantiateEnemyAtPosition(point);
         }
 
         yield return new WaitForSeconds(frequencyToSpawnNewEnemy);
+        
         StartCoroutine(SpawnEnemy());
     }
 
-    bool TryCreateRandomPoint(out Vector3 result)
+    private void ValidateSpawnFrequency()
     {
-        for (int i = 0; i < 30; i++)
+        if (frequencyToSpawnNewEnemy <= 0)
         {
-            Vector3 randomPoint = Random.insideUnitSphere;
+            frequencyToSpawnNewEnemy = 1;
+        }
+    }
 
-            randomPoint.x *= playerBoundsGenerator.width/2;
-            randomPoint.y *= playerBoundsGenerator.height/2;
+    bool TryToFindRandomPointInWorldToSpawn(out Vector3 result)
+    {
+        int attemps = 30;
+        for (int i = 0; i < attemps; i++)
+        {
+            Vector3 randomPoint = GenerateRandomPoint();
             
-            NavMeshHit hit;
-
-            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+            Vector3 spawnPosition;
+            
+            if (TrySampleNavMesh(randomPoint, out spawnPosition) && canSpawn(randomPoint))
             {
-                if(Vector3.Distance(randomPoint, playerTf.position) >= minSpawnDistanceToPlayer && !IsPositionOccupied(randomPoint))
-                {
-                    result = hit.position;
-                    return true;
-                }
-                
+                result = spawnPosition;
+                return true;                        
             }
         }
+
         result = Vector3.zero;
         return false;
     }
 
-
-    bool IsPositionOccupied(Vector3 position)
+    bool TrySampleNavMesh(Vector3 point, out Vector3 hitPosition)
     {
-        //Try near points around choosen random point to see if place is occupied.
+        NavMeshHit navMeshHit;
+        bool success = NavMesh.SamplePosition(point, out navMeshHit, 1.0f, NavMesh.AllAreas);
+
+        hitPosition = success ? navMeshHit.position : Vector3.zero;
+        return success;
+    }
+
+    void InstantiateEnemyAtPosition(Vector3 point)
+    {
+        Instantiate(enemyTypes[RandomIndexEnemyPrefab()], point, RandomRotationAroundZ());
+    }
+
+    int RandomIndexEnemyPrefab()
+    {
+        float randomIndexFloat = Random.Range(0, enemyTypes.Count);
+        int randomIndex = Mathf.FloorToInt(randomIndexFloat);
+        
+        return randomIndex;
+    }
+    Vector3 GenerateRandomPoint()
+    {
+        Vector3 randomPoint = Random.insideUnitSphere;
+
+        randomPoint.x *= playerBoundsGenerator.width / 2;
+        randomPoint.y *= playerBoundsGenerator.height / 2;
+
+        return randomPoint;
+    }
+
+    bool canSpawn(Vector3 randomPoint)
+    {
+        bool farEnoughFromPlayer = Vector3.Distance(randomPoint, playerTf.position) >= minSpawnDistanceToPlayer;
+        return farEnoughFromPlayer && !IsPositionOccupiedByAnyEntity(randomPoint);
+    }
+
+    Quaternion RandomRotationAroundZ()
+    {
+        return Quaternion.Euler(0, 0, Random.Range(0f, 360f));
+    }
+
+
+    bool IsPositionOccupiedByAnyEntity(Vector3 position)
+    {
+        //Try near points around choosen random point to see if place is occupied by some entity.
         int ignorelayerMask = ~LayerMask.GetMask("Untagged");
         
-        RaycastHit hit;
-        if (Physics.Raycast(position + Vector3.forward * 0.1f, Vector3.up, out hit, 1.0f, ignorelayerMask))
+        if (Physics.Raycast(position + Vector3.forward * 0.1f, Vector3.up, 1.0f, ignorelayerMask))
         {
             return true;
         }
 
-        if (Physics.Raycast(position + Vector3.forward * 0.1f + Vector3.right * 0.5f, Vector3.up, out hit, 1.0f, ignorelayerMask))
+        if (Physics.Raycast(position + Vector3.forward * 0.1f + Vector3.right * 0.5f, Vector3.up, 1.0f, ignorelayerMask))
         {
             return true;
         }
 
-        if (Physics.Raycast(position + Vector3.forward * 0.1f - Vector3.right * 0.5f, Vector3.up, out hit, 1.0f, ignorelayerMask))
+        if (Physics.Raycast(position + Vector3.forward * 0.1f - Vector3.right * 0.5f, Vector3.up, 1.0f, ignorelayerMask))
         {
             return true;
         }
 
-        if (Physics.Raycast(position + Vector3.forward * 0.1f + Vector3.up * 0.5f, Vector3.up, out hit, 1.0f, ignorelayerMask))
+        if (Physics.Raycast(position + Vector3.forward * 0.1f + Vector3.up * 0.5f, Vector3.up, 1.0f, ignorelayerMask))
         {
             return true;
         }
 
-        if (Physics.Raycast(position + Vector3.forward * 0.1f - Vector3.up * 0.5f, Vector3.up, out hit, 1.0f, ignorelayerMask))
+        if (Physics.Raycast(position + Vector3.forward * 0.1f - Vector3.up * 0.5f, Vector3.up, 1.0f, ignorelayerMask))
         {
             return true;
         }
