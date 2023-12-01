487fc198592b579386c5a8fa8baee6cedb09e9c6
unknown <murilo63793@gmail.com>
2023-12-01 10:24:30 -0300

    refactor: change for a descriptive name

--- Assets/Canvas/Buttons/Scripts/AutoSelectButtonOnEnable.cs
+++ Assets/Canvas/Buttons/Scripts/AutoSelectButtonOnEnable.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StarterSelectedButtonOnEnable : MonoBehaviour
+public class AutoSelectButtonOnEnable : MonoBehaviour
 {
     Button button;
 
